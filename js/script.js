04309bf3e7fc259a0d9f512c345216c0eee130cf
JeKhu <khujerry02@gmail.com>
2022-07-03 22:10:12 +0700

    another content, color theme and light dark theme

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing", {
+    strings: ["", "Web Developer", "Web Designer", "Full-Stack Web Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
---
+++ js/style-switch.js
@@ -0,0 +1,41 @@
+/* Toggle Style Switch */
+const styleSwitcherToggle = document.querySelector(".style-switch-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switch").classList.toggle("open");
+})
+
+// Hide style switch on scroll
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switch").classList.contains("open")) {
+        document.querySelector(".style-switch").classList.remove("open");
+    }
+})
+
+/* Theme color */
+const alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternateStyle.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        }
+        else {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+/* Light-Dark */
+const dayNight = document.querySelector(".night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
